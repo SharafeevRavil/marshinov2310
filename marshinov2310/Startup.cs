8494384364215789c9054e381cc0f74bc062c510
Ravil <graf.rav@gmail.com>
2019-10-23 18:30:55 +0300

    zhopa

--- marshinov2310/Startup.cs
+++ marshinov2310/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace WebApplication
+namespace marshinov2310
 {
     public class Startup
     {
